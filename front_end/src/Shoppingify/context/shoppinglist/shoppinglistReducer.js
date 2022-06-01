92655a8dd79212a9ecf2f7453e1f80673e9677f1
Craxy_Jesi <39983328+jesintharnold@users.noreply.github.com>
2022-06-02 04:05:17 +0530

    context switch states completed

--- front_end/src/Shoppingify/context/shoppinglist/shoppinglistReducer.js
+++ front_end/src/Shoppingify/context/shoppinglist/shoppinglistReducer.js
@@ -2,46 +2,91 @@ import {GET_ITEMS_LIST,
   ADD_ITEM_LIST,
   DELETE_ITEM_LIST,
   CHECK_ITEM_LIST,
-  UNCHECK_ITEM_LIST,
   SET_NAME_LIST,
   LIST_STATUS_LIST} from '../dispatchactions';
 
 //Schema - design
+
 // {
 //   items:[
 //     {
-//       category:"A",
-//       item_name:"Curd",
-//       qunantity:1
-//     }
+//       category:"Fruit and vegetables",
+//       categoryID:"xxxxxxxxxx",
+//       items:[
+//         {name:"Avocodo",quantity:5,itemID:"xxxxx",checked:false},
+//         {name:"Avocodo bell",quantity:5,itemID:"xxxxx",checked:false},
+//         {name:"Avocodo Laptop",quantity:5,itemID:"xxxxx",checked:false}
+//         ]
+//       }
 //   ],
-//   status:true,
-//   name:null
+//   listStatus:00001000,
+//   listName:null
 // }
+
+
+
 export const shoppinglistreducer=(state,action)=>{
  
 switch(action.type){
   case GET_ITEMS_LIST:
     //fetch and place it
-    return;
+    return {...action.payload};
   case ADD_ITEM_LIST:
     //cross-check with category and value , if exists ++ else add new
-    return;
+     
+    let category_exist=state.items.find(({categoryID})=>categoryID==action.payload.categoryID);
+    let item_exist=category_exist.find(({itemID})=>itemID==action.payload.itemID);
+     
+    if(category_exist){
+      if(item_exist){
+        return {...state,items:state.items.map((el)=>(el.categoryID===action.payload.categoryID)?
+            {...el,items:el.items.map((iel)=>(iel.itemID===action.payload.itemID)?iel.quantity+=1:iel)}
+            :el)}
+      }else{
+        return {...state,items:state.items.map((el)=>(el.categoryID===action.payload.categoryID)?
+          {...el,items:[...el.items,{
+            name:action.payload.name,
+            quantity:1,
+            itemID:action.payload.itemID,
+            checked:false
+          }]}
+          :el)}
+      }
+    }else{
+        return {...state,items:[...state.items,{
+          category:action.payload.category,
+          categoryID:action.payload.categoryID,
+          items:[
+            {name:action.payload.name,quantity:1,itemID:action.payload.itemID,checked:false}
+            ]
+          }
+        ]}
+      }
+
   case DELETE_ITEM_LIST:
-    //cross-check with category and value , if exists -- else add new
-    return;
+    //cross-check with category and value get the count , if exists -- else add new
+
+    let item_cnt=state.items.filter(({categoryID})=>categoryID===action.payload.categoryID)[0].items.length;
+    if(item_cnt===1){
+      return {...state,items:state.items.filter(({categoryID})=>categoryID!==action.payload.categoryID)}
+    }else{
+      return {...state,items:state.items.map((el)=>(el.categoryID===action.payload.categoryID)?
+        {...el,items:el.items.filter(({itemID})=>itemID!==action.payload.itemID)}
+        :el)}
+    }
+
   case CHECK_ITEM_LIST:
     //cross-check with category and value and make check as true/false
-    return;
-  case UNCHECK_ITEM_LIST:
-    //cross-check with category and value and make check as true/false
-    return;
+    return {...state,items:state.items.map((el)=>(el.categoryID===action.payload.categoryID)?
+      {...el,items:el.items.map((iel)=>(iel.itemID===action.payload.itemID)?iel.checked=action.payload.checked:iel)}
+      :el)};
+
   case SET_NAME_LIST:
     //set name to the list by default it is null
-    return;
+    return {...state,listName:action.payload.name};
   case LIST_STATUS_LIST:
     // Set status as active|true(completed)|false(cancelled)  
-    return;        
+    return {...state,listStatus:action.payload.status};        
   default:
     return state; 
 }
